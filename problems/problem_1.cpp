14a658db74ed8f280c5aefbf11b856cb96886330
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-13 12:06:14 +0500

    1

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,4 +1,4 @@
-void problemSolution1(float consumed_water) {
+float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
     // cost + consumed_water * 0.4
@@ -10,7 +10,8 @@ void problemSolution1(float consumed_water) {
     }
     if (consumed_water <= 60 ){
         cost =  13 + 30 *0.4 + 20 * 0.12 + (consumed_water-50)*1.4;
-    } else {
+    }
+    if (consumed_water > 60){
         cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) *1.5;
     }
 
