0b532ed8489f94637344ae9c905c878e01adde63
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-13 12:13:09 +0500

    1

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,16 +2,17 @@ float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
     // cost + consumed_water * 0.4
+
     if (consumed_water <= 30 ){
         cost =  13 + consumed_water * 0.4;
     }
-    if (consumed_water <= 50 ){
+    if (30 < consumed_water <= 50 ){
         cost =  13 + 30 * 0.4 + (consumed_water - 30) * 0.12;
     }
-    if (consumed_water <= 60 ){
+    if (50 < consumed_water <= 60 ){
         cost =  13 + 30 *0.4 + 20 * 0.12 + (consumed_water-50)*1.4;
     }
-    if (consumed_water > 60){
+    if (60 < consumed_water > 60){
         cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) *1.5;
     }
 
