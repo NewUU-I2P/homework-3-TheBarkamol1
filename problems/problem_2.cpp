c55e4e1f77d4d6be54d7032e9825b88c1f665a81
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-13 12:16:49 +0500

    1

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,8 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
+    result = (i > j) ? j : k;
+    result = (result > k) ? result : k;
 
     return result;
 }
