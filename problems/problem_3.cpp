de28b9172e0d34d9c53cc02200e01b56a8a335ec
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-13 19:39:59 +0500

    1

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,9 +1,27 @@
 #include <string>
 
-std::string problemSolution3(float height, char S) {
+std::string problemSolution3(float height, char S, ) {
+    std::string result;
     // write your code here
+    if (S == "male") {
+        if (height < 1.70) {
+            result = "Short";
+        } else if (height >= 1.70 && height < 1.85) {
+            result = "Normal";
+        } else {
+            result = "Tall";
+        }
+    } else if (S == "female") {
+        if (height < 1.60) {
+            result = "Short";
+        } else if (height >= 1.60 && height < 1.75) {
+            result = "Normal";
+        } else {
+            result = "Tall";
+        }
+    }
 
-
+    return result;
     // use return to return your result
     // make use of control flow statements
 }
