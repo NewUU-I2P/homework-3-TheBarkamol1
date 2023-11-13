5c3385791036cbae364b6d6468d7e64a95caac35
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-13 23:08:20 +0500

    1

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,27 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
+    switch (operation) {
+        case '+':
+            result = x + y;
+            break;
+        case '-':
+            result = x - y;
+            break;
+        case '*':
+            result = x * y;
+            break;
+        case '/':
+            if (y == 0) {
+
+                return 0;
+            }
+            result = x / y;
+            break;
+        default:
+            return 0;
+    }
+
 
    return result;
 }
