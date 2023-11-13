71b24cff60805ff2fee13639b926aa39a17413c3
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-13 23:04:33 +0500

    1

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,25 +3,6 @@
 std::string problemSolution3(float height, char S) {
     std::string result;
     // write your code here
-    if (S == 1) {
-        if (height < 1.70) {
-            result = "Short";
-        } else if (height >= 1.70 && height < 1.85) {
-            result = "Normal";
-        } else {
-            result = "Tall";
-        }
-    }
-    if (S == 2) {
-        if (height < 1.60) {
-            result = "Short";
-        } else if (height >= 1.60 && height < 1.75) {
-            result = "Normal";
-        } else {
-            result = "Tall";
-        }
-    }
 
-    return result;
 }
 
