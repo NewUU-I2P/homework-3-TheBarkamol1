dcc88a34fb0a0275e16be331e8e3f9f5ace5de8e
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-14 07:42:36 +0500

    1

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,24 +3,7 @@
 std::string problemSolution3(float height, char S) {
     std::string result;
     // write your code here
-    if (S == "male") {
-        if (height < 1.70) {
-            result = "Short";
-        } else if (height >= 1.70 && height < 1.85) {
-            result = "Normal";
-        } else {
-            result = "Tall";
-        }
-    }
-    if (S = "female") {
-        if (height < 1.60) {
-            result = "Short";
-        } else if (height >= 1.60 && height < 1.75) {
-            result = "Normal";
-        } else {
-            result = "Tall";
-        }
-    }
+
 
     return result;
 }
--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,9 +3,25 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    int n = 0;
+    std::string result;
+     if (macAddress[0] == 'F' and macAddress[1] == 'F' ){
+         result = "Broadcast";
 
+     }else {
+         n = (macAddress[1] - '0');
+
+         if(n > 9) n ++;
+
+         if (n % 2 == 0) {
+             result = "Unicast";
+
+         } else {
+             result = "Multicast";
+            }
+         }
+     return result;
 
     // make use of control flow statements
     // return result;
-    return result;
 }
