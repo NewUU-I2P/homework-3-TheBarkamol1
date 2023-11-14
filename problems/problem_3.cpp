03f470f368151a71abc8c274648b8b55a1cf6d99
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-14 17:13:24 +0500

    1

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,11 +2,8 @@
 
 std::string problemSolution3(float height, char s) {
     std::string result;
-    std::string M = "male";
-    std::string F = "female";
-
     // write your code her
-    if (s=='M'){
+    if (s=="Male"){
         if(height < 1.70){
             result = "short";
         } if (1.70 <= height < 1.85){
@@ -14,7 +11,7 @@ std::string problemSolution3(float height, char s) {
         } if (height >= 1.85){
             result = "Tall";
         }
-    } if(s=='F'){
+    } if(s=="Female"){
         if(height < 1.60){
             result = "short";
         } if (1.60 <= height < 1.75){
