d82d390cd0cb03049daed43ecc6533572de750ce
Barkamol <b.abdumavlonov@newuu.uz>
2023-11-14 17:24:55 +0500

    1

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,28 +3,29 @@
 std::string problemSolution3(float height, char s) {
     std::string result;
     // write your code her
-    if (s=='M'){
-        if(height < 1.70){
-            return "short";
-        } if (1.70 <= height < 1.85){
-            return "normal";
-        } if (height >= 1.85){
+    if (s == 'M') {
+        if (height < 1.70) {
+            return "Short";
+        } else if (height < 1.85) {
+            return "Normal";
+        } else {
             return "Tall";
         }
-    } if(s=='F'){
-        if(height < 1.60){
-            return "short";
-        } if (1.60 <= height < 1.75){
-            return "normal";
-        } if (height >= 1.75){
+    } else if (s == 'F') {
+        if (height < 1.60) {
+            return "Short";
+        } else if (height < 1.75) {
+            return "Normal";
+        } else {
             return "Tall";
         }
-
-
     }
+    return "Invalid sex";
+
     
 
 
     return result;
 }
+
 
